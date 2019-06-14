67e01645c9a5ddf0bb04fb726595496f52e9946c
drnick <you@example.com>
2019-06-14 23:42:48 +0200

    sdfgdsfg

--- resources/js/gallery.js
+++ resources/js/gallery.js
@@ -247,10 +247,6 @@ var photoBooth = (function () {
         public.openGallery($(this));
     });
 
-    $(window).load(function () {
-       public.openGallery($(this));
-    });
-
     // Close Gallery Overview
     $('.close_gal').click(function (e) {
         e.preventDefault();
