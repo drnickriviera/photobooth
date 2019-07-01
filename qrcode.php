ea1c2e0a536945a7a3a8c4a42c2cfe8643d258b1
drnick <you@example.com>
2019-07-01 21:25:30 +0200

    add ip manually

--- qrcode.php
+++ qrcode.php
@@ -2,5 +2,5 @@
 $filename = $_GET['filename'];
 include('resources/lib/phpqrcode/qrlib.php');
 $iphost = getHostByName(getHostName());
-$url = 'http://'.$iphost.'/download.php?image=';
+$url = 'http://10.42.0.1/download.php?image=';
 QRcode::png($url.$filename, false, QR_ECLEVEL_H, 10);
